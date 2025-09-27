a621577d5ffc26384c142d736e442ffcdc3334de
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-09-27 22:38:57 +0530

    /practice/list practice cards done

--- src/practiceList/practiceList.js
+++ src/practiceList/practiceList.js
@@ -1,7 +1,9 @@
 import Navbar from "../navbar/navbar";
+import practiceCards from "../home/practiceCards";
 
 export default function practiceList() {
 
     Navbar();
-
+    practiceCards();
+    
 }
